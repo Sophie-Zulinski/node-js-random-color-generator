a0615c8694ca90bbbacc331289c286ab397d63a8
Sophie-Zulinski <sophie.zulinski@gmx.at>
2023-01-10 20:24:24 +0100

    Added index.js

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-import chalk from chalk;
-import randomColor from randomColor;
-
-
-const color = randomColor();
-function colorAndLog() {
-  console.log(chalk.hex(color));
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+const lum = process.argv[3];
+const col = process.argv[2];
+const color = randomColor({
+  luminosity: lum,
+  hue: col,
+});
+function colorAndLog(str) {
+  console.log(chalk.hex(color).bold(str));
 }
+// LONG VERSION
+// colorAndLog(“##############################“);
+// colorAndLog(“##############################“);
+// colorAndLog(“##############################“);
+// colorAndLog(“########              ########“);
+// colorAndLog(`########   ${color}    ########`);
+// colorAndLog(“########              ########“);
+// colorAndLog(“##############################“);
+// colorAndLog(“##############################“);
+// colorAndLog(“##############################“);
+// SHORT VERSION
+colorAndLog(`##############################
+##############################
+##############################
+########              ########
+########   ${color}    ########
+########              ########
+##############################
+##############################
+##############################`);
+
 
 
